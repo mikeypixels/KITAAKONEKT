b2e1bb327b644d6aad4435e5f6efc501000d2ec9
siddy <saidimmaka@gmail.com>
2020-03-22 02:29:19 +0300

    expandable list new

--- app/src/main/java/com/justai/aimybox/assistant/KnowYourself.kt
+++ app/src/main/java/com/justai/aimybox/assistant/KnowYourself.kt
@@ -21,67 +21,19 @@ class KnowYourself : AppCompatActivity() , View.OnClickListener {
     internal var adapter: ExpandableListAdapter? = null
     internal var titleList: List<String> ? = null
 
-    private val data: HashMap<String, List<String>>
-
-        get() {
-            val listData = HashMap<String, List<String>>()
-
-            val redmiMobiles = ArrayList<String>()
-            redmiMobiles.add("Redmi Y2")
-            redmiMobiles.add("Redmi S2")
-            redmiMobiles.add("Redmi Note 5 Pro")
-            redmiMobiles.add("Redmi Note 5")
-            redmiMobiles.add("Redmi 5 Plus")
-            redmiMobiles.add("Redmi Y1")
-            redmiMobiles.add("Redmi 3S Plus")
-
-            val micromaxMobiles = ArrayList<String>()
-            micromaxMobiles.add("Micromax Bharat Go")
-            micromaxMobiles.add("Micromax Bharat 5 Pro")
-            micromaxMobiles.add("Micromax Bharat 5")
-            micromaxMobiles.add("Micromax Canvas 1")
-            micromaxMobiles.add("Micromax Dual 5")
-
-            val appleMobiles = ArrayList<String>()
-            appleMobiles.add("iPhone 8")
-            appleMobiles.add("iPhone 8 Plus")
-            appleMobiles.add("iPhone X")
-            appleMobiles.add("iPhone 7 Plus")
-            appleMobiles.add("iPhone 7")
-            appleMobiles.add("iPhone 6 Plus")
-
-            val samsungMobiles = ArrayList<String>()
-            samsungMobiles.add("Samsung Galaxy S9+")
-            samsungMobiles.add("Samsung Galaxy Note 7")
-            samsungMobiles.add("Samsung Galaxy Note 5 Dual")
-            samsungMobiles.add("Samsung Galaxy S8")
-            samsungMobiles.add("Samsung Galaxy A8")
-            samsungMobiles.add("Samsung Galaxy Note 4")
-
-            listData["Redmi"] = redmiMobiles
-            listData["Micromax"] = micromaxMobiles
-            listData["Apple"] = appleMobiles
-            listData["Samsung"] = samsungMobiles
-
-            return listData
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.know_yourself_layout)
 
-        val toolbarr : Toolbar = findViewById(R.id.tool_bar2)
-        toolbarr.title = ""
-        setSupportActionBar(toolbarr)
+        val toolbar : Toolbar = findViewById(R.id.tool_bar2)
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
 
         // Now get the support action bar
         val actionBar = supportActionBar
 
-//        getActionBar()?.elevation = 4.0F
         actionBar?.setIcon(R.drawable.ic_chevron_left_gray_24dp)
-//        actionBar?.setDisplayShowTitleEnabled(true)
-//        actionBar?.setDisplayShowHomeEnabled(true)
-
 
         expandableListView = findViewById(R.id.expandableLayout1)
         expandableButton = findViewById(R.id.btn1)
