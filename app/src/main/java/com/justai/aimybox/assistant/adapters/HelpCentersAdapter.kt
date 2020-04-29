d46fbced212e5da613d3b3581cb411b7eba1675a
mikey <mikaelapina1@gmail.com>
2020-04-29 14:25:46 +0300

    12th commit

--- app/src/main/java/com/justai/aimybox/assistant/adapters/HelpCentersAdapter.kt
+++ app/src/main/java/com/justai/aimybox/assistant/adapters/HelpCentersAdapter.kt
@@ -1,10 +1,14 @@
 package com.justai.aimybox.assistant.adapters
 
 import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,11 +22,17 @@ import com.justai.aimybox.assistant.HelpCenter
 import com.justai.aimybox.assistant.R
 import com.justai.aimybox.assistant.activities.MapsActivity
 
-class HelpCentersAdapter(context: Context, helpCenters: ArrayList<HelpCenter>) : RecyclerView.Adapter<HelpCentersAdapter.ViewHolder>()  {
+
+class HelpCentersAdapter(context: Context, helpCenters: ArrayList<HelpCenter>) :
+    RecyclerView.Adapter<HelpCentersAdapter.ViewHolder>() {
 
     var context: Context
 
     var helpCenters: ArrayList<HelpCenter>
+
+    private val REQUEST_PERMISSION_PHONE_STATE = 1
+
+    private val MAKE_CALL_PERMISSION_REQUEST_CODE = 1
 
     init {
         this.context = context
@@ -57,33 +67,26 @@ class HelpCentersAdapter(context: Context, helpCenters: ArrayList<HelpCenter>) :
         holder.help_name.text = helpCenters.get(position).name
         holder.location.text = helpCenters.get(position).location
         holder.phone_no.text = helpCenters.get(position).phone_no
-        if(helpCenters.get(position).phone_no == "Unk"){
+        if (helpCenters.get(position).phone_no == "Unk") {
             holder.phone_no.setTextColor(context.resources.getColor(R.color.red))
-        }else{
+        } else {
             holder.phone_no.setTextColor(context.resources.getColor(R.color.blue))
         }
 
-        holder.phone_no.setOnClickListener{
-            val intent = Intent(Intent.ACTION_CALL)
-            intent.data = Uri.parse("tel:" + "0712957994")
-
-            Toast.makeText(context, "Just inside the listener", Toast.LENGTH_SHORT).show()
-            if (ContextCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.CALL_PHONE
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return@setOnClickListener
+        holder.phone_no.setOnClickListener {
+            if (checkPermission(Manifest.permission.CALL_PHONE)) {
+                holder.phone_no.setEnabled(true)
+                val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + holder.phone_no.text.toString()))
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                context.startActivity(intent)
+            } else {
+                holder.phone_no.setEnabled(false)
+                ActivityCompat.requestPermissions(
+                    context as Activity,
+                    arrayOf(Manifest.permission.CALL_PHONE),
+                    MAKE_CALL_PERMISSION_REQUEST_CODE
+                )
             }
-            Toast.makeText(context, "after the listener", Toast.LENGTH_SHORT).show()
-            context.startActivity(intent)
         }
 
         holder.map_img.setOnClickListener {
@@ -91,5 +94,51 @@ class HelpCentersAdapter(context: Context, helpCenters: ArrayList<HelpCenter>) :
             context.startActivity(intent)
         }
     }
+
+    private fun checkPermission(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            context,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+//    private fun callPhone(phone: String) {
+//        val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone))
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//        if (ActivityCompat.checkSelfPermission(
+//                context,
+//                Manifest.permission.CALL_PHONE
+//            ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            ActivityCompat.requestPermissions(context as Activity, String[]{Manifest.permission.CALL_PHONE}, MAKE_CALL_PERMISSION_REQUEST_CODE);
+//            return
+//        }
+//        context.startActivity(intent)
+
+//    }
+
+
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<String?>?,
+//        grantResults: IntArray
+//    ) {
+//        when (requestCode) {
+//            REQUEST_PERMISSION_PHONE_STATE -> if (grantResults.size > 0
+//                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+//            ) {
+//                Toast.makeText(context, "Permission Granted!", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(context, "Permission Denied!", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
 
 }
--- app/src/main/java/com/justai/aimybox/assistant/fragments/HelpCentersFragment.kt
+++ app/src/main/java/com/justai/aimybox/assistant/fragments/HelpCentersFragment.kt
@@ -1,18 +1,22 @@
 package com.justai.aimybox.assistant.fragments
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.hanks.htextview.evaporate.EvaporateTextView
 import com.justai.aimybox.assistant.HelpCenter
-
 import com.justai.aimybox.assistant.R
 import com.justai.aimybox.assistant.adapters.HelpCentersAdapter
 import com.justai.aimybox.assistant.utils.ExitWithAnimation
 import com.justai.aimybox.assistant.utils.startCircularReveal
+
 
 class HelpCentersFragment : Fragment(), ExitWithAnimation {
 
@@ -20,6 +24,10 @@ class HelpCentersFragment : Fragment(), ExitWithAnimation {
     override var posY: Int? = null
 
     var helpCentersArray: ArrayList<HelpCenter> = ArrayList()
+    var arrMessages: ArrayList<String> = ArrayList()
+    var position = 0
+    var delay = 4000 //milliseconds
+
 
     override fun isToBeExitedWithAnimation(): Boolean = true
 
@@ -41,9 +49,27 @@ class HelpCentersFragment : Fragment(), ExitWithAnimation {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_help_centers, container, false)
 
+        val evap_txt: EvaporateTextView = view.findViewById(R.id.evap_txt)
         val recyclerView: RecyclerView = view.findViewById(R.id.help_centers_recyclerView)
         val linearLayoutManager = LinearLayoutManager(context)
         recyclerView.layoutManager = linearLayoutManager
+
+        arrMessages.add("Je unahitaji msaada?")
+        arrMessages.add("Bofya namba ya simu kupiga!")
+
+        evap_txt.animateText(arrMessages.get(position))
+        position++
+
+        val handler = Handler()
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                handler.postDelayed(this, delay.toLong())
+                if (position >= arrMessages.size)
+                    position = 0
+                evap_txt.animateText(arrMessages.get(position))
+                position++
+            }
+        }, delay.toLong())
 
         helpCentersArray.add(HelpCenter("Benjamin Mkapa Hospital", "Dodoma", "026 296 3710"))
         helpCentersArray.add(HelpCenter("Aga Khan Hospital", "Dodoma", "026 232 1789"))
