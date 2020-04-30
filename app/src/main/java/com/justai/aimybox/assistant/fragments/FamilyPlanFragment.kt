4cee669e82b8f045210e4354d728fb62a91cbd1f
siddy <saidimmaka@gmail.com>
2020-04-30 23:48:06 +0300

    family planning permanent contraceptions

--- app/src/main/java/com/justai/aimybox/assistant/fragments/FamilyPlanFragment.kt
+++ app/src/main/java/com/justai/aimybox/assistant/fragments/FamilyPlanFragment.kt
@@ -1,17 +1,22 @@
 package com.justai.aimybox.assistant.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.github.aakira.expandablelayout.ExpandableLinearLayout
 import com.justai.aimybox.assistant.R
 import com.justai.aimybox.assistant.utils.ExitWithAnimation
 
 class FamilyPlanFragment : Fragment(), ExitWithAnimation {
 
+    private var expPermanentContraLayout: ExpandableLinearLayout? = null
+    lateinit var btnPermanentContra: Button
 
     override var posX: Int? = null
     override var posY: Int? = null
@@ -48,6 +53,55 @@ class FamilyPlanFragment : Fragment(), ExitWithAnimation {
         val actionBar = (activity as AppCompatActivity?)!!.supportActionBar
         actionBar?.setIcon(R.drawable.ic_chevron_left_gray_24dp)
 
+        //Buttons
+        btnPermanentContra = view.findViewById(R.id.btnPermanentContraception)
+
+        //Expendable buttons and Layouts
+        expPermanentContraLayout = view.findViewById(R.id.permanentContraLayout)
+        btnPermanentContra.setOnClickListener{
+            onClick(btnPermanentContra)
+        }
+
         return view
+    }
+
+    private fun initmyLayout() {
+        expPermanentContraLayout?.initLayout()
+//        expReproductiveOrgaLayout?.initLayout()
+//        expSemenLayout?.initLayout()
+//        expSpermLayout?.initLayout()
+//        expHormonesLayout?.initLayout()
+    }
+
+    private fun onClick(v: View?) {
+
+        if (v != null) {
+            when (v.id) {
+                R.id.btnPermanentContraception -> {
+
+                    Log.d("Family plan", "Button clicked")
+                    expPermanentContraLayout?.toggle()
+//                    expReproductiveOrgaLayout?.collapse()
+//                    expSemenLayout?.collapse()
+                }
+
+                R.id.btnTemporaryContraception -> {
+//                    expReproductiveOrgaLayout?.toggle()
+//                    expPubertyLayout?.collapse()
+//                    expSemenLayout?.collapse()
+                }
+
+                R.id.btnEmergencyContraception -> {
+//                    expSemenLayout?.toggle()
+//                    expPubertyLayout?.collapse()
+//                    expReproductiveOrgaLayout?.collapse()
+                }
+
+                else -> {
+
+                }
+            }
+        }
+        initmyLayout()
     }
 }
